42b92a7fb1aaa869e883114a8b2163e180a4c844
JofielRamirez <jofiel.ramirez@uabc.edu.mx>
2026-06-13 02:58:19 -0700

    Assignment 3

--- app/src/main/java/com/fixnow/soundinch8/MainActivity.kt
+++ app/src/main/java/com/fixnow/soundinch8/MainActivity.kt
@@ -8,9 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.fixnow.soundinch8.ui.screens.LoginScreen
+import com.fixnow.soundinch8.ui.screens.RegisterScreen
 import com.fixnow.soundinch8.ui.theme.SoundInCh8Theme
 
 class MainActivity : ComponentActivity() {
@@ -24,9 +29,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen(onNavigateToRegister = {})
+                    var currentScreen by remember { mutableStateOf("login") }
 
-                    // Navigation goes here
+                    when (currentScreen) {
+                        "login" -> {
+                            LoginScreen(
+                                onNavigateToRegister = {
+                                    currentScreen = "register"
+                                }
+                            )
+                        }
+
+                        "register" -> {
+                            RegisterScreen(
+                                onGoBack = {
+                                    currentScreen = "login"
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
